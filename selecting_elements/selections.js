ed0181ca6be6a365c7f5158199d52c8493c2846d
Josh Archer <jarcher@greenriver.edu>
2022-01-10 10:50:42 -0800

    Added a few more examples of selecting elements on a web page.

--- selecting_elements/selections.js
+++ selecting_elements/selections.js
@@ -9,3 +9,17 @@ message.innerText = "Thanks for stopping by my webpage";
 
 //add to the text in the element
 message.innerText += "This page was created by Josh";
+
+//alternatively, select elements using a "CSS Selector"
+let orderedList = document.querySelector("ol.messages");
+orderedList.innerHTML = "<li>Hello, how are you?</li>";
+
+let favorite = document.querySelector("#hobbies-list li:first-child");
+favorite.style.border = "2px solid pink";
+
+//example of magic numbers
+let width = favorite.clientWidth / 2 + 50;
+
+//use the width variable
+let height = someValue + 50;
+
