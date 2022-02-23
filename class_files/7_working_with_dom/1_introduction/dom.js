c37061b5ff6143e30678da6c1863fe12bd86b211
Josh Archer <jarcher@greenriver.edu>
2022-02-23 10:08:47 -0800

    Added examples of selecting and manipulating multiple dom elements.

--- class_files/7_working_with_dom/1_introduction/dom.js
+++ class_files/7_working_with_dom/1_introduction/dom.js
@@ -1,14 +1,65 @@
-//select the body element
-let body = document.body;
-console.log(body.tagName);  //body
-console.log(body.nodeType); //1
+//select elements that are paragraphs
+let paragraphs = document.querySelectorAll("p.even");
 
-//select all elements under body (in an array like structure)
-let elements = document.querySelectorAll("*");
-for (let i = 0; i < elements.length; i++)
+//print to the console for debugging purposes
+console.log(paragraphs);
+
+//loop over the elements in the NodeList (an array-like structure)
+/*for (let i = 0; i < paragraphs.length; i++)
 {
-    let element = elements[i];
-    console.log(element.tagName);  //???
-    console.log(element.nodeType); //1
+    let para = paragraphs[i];
+
+    //make a change to the paragraph
+    para.style.backgroundColor = "lightblue";
+}*/
+
+//select all article elements on the page and give them a border
+let articles = document.querySelectorAll("body > article");
+
+//sanity check!
+console.log(articles);
+
+/*for (let i = 0; i < articles.length; i++)
+{
+    articles[i].style.border = "1px solid black";
+    articles[i].style.margin = "6px";
+}*/
+
+//select all elements that share a tag name - p, a, span, div, ...
+let divs = document.getElementsByTagName("div");
+
+//select all elements that have a common class name
+let oddParas = document.getElementsByClassName("odd");
+
+/*addBackgroundColor(paragraphs, "lightgreen");
+addBackgroundColor(articles, "lightyellow");*/
+addBorder(articles, 2, "darkblue");
+addBackgroundColor(divs, "lightyellow");
+addBorder(oddParas, 1, "black");
+
+function addBackgroundColor(array, color)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        array[i].style.backgroundColor = color;
+    }
 }
+
+function addBorder(array, size, color)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        //border property looks like - 2px solid black
+        array[i].style.border = size + "px solid " + color;
+        array[i].style.margin = "10px";
+    }
+}
+
+
+
+
+
+
+
+
 
